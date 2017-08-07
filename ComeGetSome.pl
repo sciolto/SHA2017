1e7405082febffe5136da0edc2f8f4f6ba08f8c5
sciolto <attanasio@gmail.com>
2017-08-07 18:03:24 +0200

    first commit

---
+++ ComeGetSome.pl
@@ -0,0 +1,87 @@
+use Data::Dumper;
+use IO::Socket::INET;
+use GD;
+use strict;
+
+our $main_offsetx = 0;
+our $main_offsety = 0;
+
+$| = 1;
+my $img_w = 80;
+my $img_h = 80;
+my $host = 'barflood.sha2017.org';
+#my $host =  'localhost';
+my $port = 2342;
+#my $port = 7777;
+my $socket = new IO::Socket::INET (
+        PeerHost => $host,
+        PeerPort => $port,
+        Proto => 'tcp',
+        Type => SOCK_STREAM,
+    );
+
+
+GD::Image->trueColor(1);
+#my $image = new GD::Image('troll-face-meme.png') or die 'no pic dude !';
+my $image = newFromPng GD::Image('troll-face-meme.png',1) or die 'no pic dude !';
+my $w = $image->width;
+my $h = $image->height;
+
+my $imageResized = new GD::Image($img_w,$img_h);
+$imageResized->copyResized($image,0,0,0,0,$img_w,$img_h,$w,$h);
+
+printImage($imageResized);
+
+#print $imageResized->png;
+#exit 0;
+
+
+sub printImage {
+    my $w = $image->width;
+    my $h = $image->height;
+    $image = @_[0];
+    while (1) {
+        for my $x(0..$w) {
+            for my $y(0..$h) {
+                #print "$x $y $w $h\n";
+                my $i = $image->getPixel($x,$y);
+                my ($r,$g,$b) = $image->rgb($i);
+                pixel($x,$y,$r,$g,$b);
+            }
+        } 
+    }
+}
+
+
+sub pixel {
+    my ($x,$y,$r,$g,$b,$a,$offsetx,$offsety) = @_;
+    if ( !defined $a )  { $a=255 };
+    if ( !defined $offsetx ) {  $offsetx=320 };
+    if ( !defined $offsety ) {  $offsety=0 };
+    $x = $x+$offsetx;
+    $y = $y+$offsety;
+    my $cmd;
+    if ($a == 255) {
+        $cmd = sprintf("PX  %d %d %02x%02x%02x\n",$x,$y,$r,$g,$b);
+        $socket->send($cmd);
+    } else {
+        $cmd = sprintf("PX  %d %d %02x%02x%02x%02x\n",$x,$y,$r,$g,$b,$a);
+        $socket->send($cmd);
+    }
+
+}
+
+
+sub rect {
+    # implement me !
+}
+
+
+sub worm {
+    # implement me !
+}
+
+sub line {
+    my ($x,$y,$r,$g,$b,$a,$offsetx,$offsety) = @_;
+    
+}
---
+++ barflood.py
@@ -0,0 +1,52 @@
+import socket
+import random  
+HOST = 'barflood.sha2017.org'
+PORT = 2342
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+send = sock.send
+
+def pixel(x,y,r,g,b,a=255,offsetx=320,offsety=0):
+  x = x + offsetx
+  y = y + offsety
+  if a == 255:
+#    print('PX %d %d %02x%02x%02x\n' % (x,y,r,g,b))
+    send('PX %d %d %02x%02x%02x\n' % (x,y,r,g,b))
+  else:
+#    print('PX %d %d %02x%02x%02x%02x\n' % (x,y,r,g,b,a))
+    send('PX %d %d %02x%02x%02x%02x\n' % (x,y,r,g,b,a))
+
+
+def rect(x,y,w,h,r,g,b):
+ for i in xrange(x,x+w):
+   for j in xrange(y,y+h):
+     pixel(i,j,r,g,b)
+
+
+
+def worm(x,y,n,r,g,b):
+  while n:
+    pixel(x,y,r,g,b,25)
+    x += random.randint(0,2)-1
+    y += random.randint(0,2)-1
+    n -= 1
+
+import time 
+from PIL import Image
+
+#im = Image.open('troll-face-meme.png').convert('RGB')
+im = Image.open('tits-or-gtfo_o_847231.jpg').convert('RGB')
+#im = Image.open('unpodifiga.jpg').convert('RGB')
+#im = Image.open('thumbsup.jpg').convert('RGB')
+im.thumbnail((80,80), Image.ANTIALIAS)
+_,_,w,h = im.getbbox()  
+
+var = 1
+while var == 1 :
+  for x in xrange(w):
+    for y in xrange(h):
+      r,g,b = im.getpixel((x,y))
+      pixel(x,y,r,g,b)
+
+
+
